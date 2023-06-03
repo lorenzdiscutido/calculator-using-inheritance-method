a1535cb05b3b8a70cd2b9d768ef063bb91c40c2e
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-06-03 17:22:39 +0800

    Create a new class that wil inherit the functions of the previous calculator and add a new function to it

--- lorenz_calculator.py
+++ lorenz_calculator.py
@@ -1,2 +1,11 @@
 from calculator import Calculator
 from user_interface import user_interface
+
+calc = Calculator()
+ui = user_interface()
+
+class lorenz_calculator(calc):
+    def conversion(self):
+        cm = float(input("Please enter a number in cm"))
+        inches = cm/2.54
+        return inches
