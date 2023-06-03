52990978cc914920beb44b11a6064379801c5870
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-06-03 18:07:34 +0800

    Recode the divide function of the new calculator to override the old one

--- lorenz_calculator.py
+++ lorenz_calculator.py
@@ -8,7 +8,14 @@ class lorenz_calculator(Calculator):
         try:
             inches = num1/2.54
             print("Your number in inches is:", inches, "in")
-        except:
+        except (ValueError, ZeroDivisionError):
             print("Invalid. Please try again")
     
-    def divide(self)
+    def divide(self,num1, num2):
+        print("")
+        print("You chose division!")
+        try:
+            quotient = num2/num1
+            print("The quotient of your number is:", quotient)
+        except (ValueError, ZeroDivisionError):
+            print("One of the number is invalid or zero. Please try again")
