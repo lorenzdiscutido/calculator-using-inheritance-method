ae18698c5627deaaf96ec2f1264f6a92f4a44154
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-06-03 16:45:40 +0800

    Rearranged and separated the objects into two diferrent file

--- calculator.py
+++ calculator.py
@@ -1,26 +1,5 @@
-#Create a class named calculator
 class Calculator:
 
-    #Show the user what to enter to use operation
-    def option(self):
-        print("Calculator operations:")
-        print("Type 1 if you want to use Addition(+)")
-        print("Type 2 if you want to use Subtraction(-)")
-        print("Type 3 if you want to use Multiplication(x)")
-        print("Type 4 if you want to use Division(/)")
-    
-    #add a function that will ask the user what operation they like to use
-    def operation(self):
-        option = input("Please select an operation:")
-        return option
-    #Add a function that ask the user to input two numbers
-    def input(self):
-        try:
-            num = float(input("Please enter your number:"))
-            return num
-        except ValueError:
-            print("Invalid. Please try again")
-    #Add a function that add the two numbers
     def add(self, num1, num2):
         print("")
         print("You chose Addition as your operation")
@@ -30,7 +9,6 @@ class Calculator:
         except ValueError:
             print("Invalid. Please try again")  
 
-    #Add a function that subtract the two numbers
     def subtract(self, num1, num2):
         print("")
         print("You chose Subtraction as your operation")
@@ -40,7 +18,6 @@ class Calculator:
         except ValueError:
             print("Invalid. Please try again")
 
-    #Add a function that multiply the two numbers
     def multiply(self, num1, num2):
         print("")
         print("You chose Multiplication as your operation")
@@ -50,7 +27,6 @@ class Calculator:
         except ValueError:
             print("Invalid. Please try again")
 
-    #Add a function that divide the two numbers
     def divide(self, num1, num2):
         print("")
         print("You chose Division as your operation")
@@ -59,22 +35,6 @@ class Calculator:
             print("The quotient of the two number is", quotient)
         except (ValueError, ZeroDivisionError):
             print("Invalid. Please try again")
-
-    #Add a function that ask the user if they want to retry the calculation
-    def again(self):
-        while True:
-            retry = input("Do you want to retry?(y/n)")
-            if retry.lower() == "n":
-                print("")
-                print("Thank you for using this calculator")
-                print("")
-                return False
-            elif retry.lower() == "y":
-                print("")
-                print("Proceeding to another calculation...")
-                return True
-            else:
-                print("Invalid input. Please try again")
-    
+ 
     
     
---
+++ user interface.py
@@ -0,0 +1,35 @@
+class user_interface():
+        
+        def option(self):
+            print("Calculator operations:")
+            print("Type 1 if you want to use Addition(+)")
+            print("Type 2 if you want to use Subtraction(-)")
+            print("Type 3 if you want to use Multiplication(x)")
+            print("Type 4 if you want to use Division(/)")
+        
+        def operation(self):
+            option = input("Please select an operation:")
+            return option
+        
+        def input(self):
+            try:
+                num = float(input("Please enter your number:"))
+                return num
+            except ValueError:
+                print("Invalid. Please try again")
+        
+        def again(self):
+            while True:
+                retry = input("Do you want to retry?(y/n)")
+                if retry.lower() == "n":
+                    print("")
+                    print("Thank you for using this calculator")
+                    print("")
+                    return False
+                elif retry.lower() == "y":
+                    print("")
+                    print("Proceeding to another calculation...")
+                    return True
+                else:
+                    print("Invalid input. Please try again")
+        
